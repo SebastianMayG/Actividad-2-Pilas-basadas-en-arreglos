88e4a27324ec1ee963b79051535a3bac505be860
SebastianMayG <144557556+SebastianMayG@users.noreply.github.com>
2023-11-19 15:43:50 -0600

    Update Main.java

--- BusquedaBinaria/Main.java
+++ BusquedaBinaria/Main.java
@@ -2,24 +2,25 @@ import java.util.Scanner;
 
 public class Main 
 {
-        public static void main(String[] args) 
+    public static void main(String[] args) 
     {
         int[] arr = new int[1000];
         Scanner input = new Scanner(System.in);
 
         for (int i = 0; i < 1000; i++)
         {
-            arr[i] = (int)(Math.random()*1000+1);
+            arr[i] = (int)(Math.random()*10000);
         }
 
         BusquedaBinaria.quickSort(arr);
         
-        for(int i = 0; i < arr.length - 1; i++)
+        for(int i = 0; i < arr.length; i++)
         {
             System.out.print(arr[i] + ", ");
         }
 
         System.out.print("\n¿Qué número desea buscar en el arreglo? ");
+
         int x = input.nextInt();
         System.out.println("Se encuentra en la posicion: " + BusquedaBinaria.busquedaBinaria (arr,x));
 
